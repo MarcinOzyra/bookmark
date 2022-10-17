822cc95696d1dfa9ecd760630797fa36861291a8
Marcin <ozi@post.pl>
2022-10-17 19:54:43 +0200

    add mobile menu

--- js/script.js
+++ js/script.js
@@ -1,10 +1,14 @@
 const tabs = document.querySelectorAll('.tab');
 const panels = document.querySelectorAll('.panel');
+
 const btn = document.getElementById('menu-btn');
 const menu = document.getElementById('menu');
 const logo = document.getElementById('logo');
 
 tabs.forEach((tab) => tab.addEventListener('click', onTabClick));
+
+btn.addEventListener('click', navToggle);
+menu.addEventListener('click', hideNav);
 
 function onTabClick(e) {
   // Deactivate all tabs
@@ -17,4 +21,22 @@ function onTabClick(e) {
   e.target.classList.add('border-softRed', 'border-b-4');
   const classString = e.target.getAttribute('data-target');
   document.getElementById('panels').getElementsByClassName(classString)[0].classList.remove('hidden');
+}
+
+function navToggle() {
+  btn.classList.toggle('open');
+  menu.classList.toggle('flex');
+  menu.classList.toggle('hidden');
+
+  if (menu.classList.contains('flex')) {
+    logo.setAttribute('src', './images/logo-bookmark-footer.svg');
+  } else {
+    logo.setAttribute('src', './images/logo-bookmark.svg');
+  }
+}
+
+function hideNav() {
+  btn.classList.toggle('open');
+  menu.classList.toggle('flex');
+  menu.classList.toggle('hidden');
 }
